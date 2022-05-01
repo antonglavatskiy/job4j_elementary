60e825557cebd99ce7a69445661ef4c4321654a0
antonglavatskiy <glavazky@yandex.ru>
2022-05-01 12:19:08 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,14 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left > right ? left : right;
+    public static int max(int first, int second) {
+        return first > second ? first : second;
     }
 
-    public static void main(String[] args) {
-        int a = 59;
-        int b = 71;
-        int max = Max.max(a, b);
-        System.out.println(max);
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(first, max(second, third, fourth));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,46 @@ public class MaxTest {
         int max = Max.max(a, b);
         Assert.assertEquals(expected, max);
     }
+
+    @Test
+    public void whenMax13To14To15Then15() {
+        int a = 13;
+        int b = 14;
+        int c = 15;
+        int expected = 15;
+        int max = Max.max(a, b, c);
+        Assert.assertEquals(expected, max);
+    }
+
+    @Test
+    public void whenMax17To14To15Then17() {
+        int a = 17;
+        int b = 14;
+        int c = 15;
+        int expected = 17;
+        int max = Max.max(a, b, c);
+        Assert.assertEquals(expected, max);
+    }
+
+    @Test
+    public void whenMax23To24To25To26Then26() {
+        int a = 23;
+        int b = 24;
+        int c = 25;
+        int d = 26;
+        int expected = 26;
+        int max = Max.max(a, b, c, d);
+        Assert.assertEquals(expected, max);
+    }
+
+    @Test
+    public void whenMax23To27To25To26Then27() {
+        int a = 23;
+        int b = 27;
+        int c = 25;
+        int d = 26;
+        int expected = 27;
+        int max = Max.max(a, b, c, d);
+        Assert.assertEquals(expected, max);
+    }
 }
