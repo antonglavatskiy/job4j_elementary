67f7e440f4e0125353df9010f0dd9cd29ae5f0d3
antonglavatskiy <glavazky@yandex.ru>
2022-05-01 11:05:36 +0500

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,33 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        double result1 = Point.distance(-5, 3, 10, 3);
-        System.out.println("result (-5, 3) to (10, 3) " + result1);
-        double result2 = Point.distance(2, -3, -7, 1);
-        System.out.println("result (2, -3) to (-7, 1) " + result2);
-
+        Point pointA = new Point(0, 0);
+        Point pointB = new Point(2, 0);
+        double distanceAB = pointA.distance(pointB);
+        System.out.println("result (0, 0) to (2, 0) " + distanceAB);
+        Point pointC = new Point(-5, 3);
+        Point pointD = new Point(10, 3);
+        double distanceCD = pointC.distance(pointD);
+        System.out.println("result (-5, 3) to (10, 3) " + distanceCD);
+        Point pointE = new Point(2, -3);
+        Point pointF = new Point(-7, 1);
+        double distanceEF = pointE.distance(pointF);
+        System.out.println("result (2, -3) to (-7, 1) " + distanceEF);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,56 +7,46 @@ public class PointTest {
 
     @Test
     public void when00to20then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point pointA = new Point(0, 0);
+        Point pointB = new Point(2, 0);
         double excepted = 2;
-        double result = Point.distance(x1, y1, x2, y2);
+        double result = pointA.distance(pointB);
         Assert.assertEquals(excepted, result, 0.01);
     }
 
     @Test
     public void when25to10then5dot09() {
-        int x1 = 2;
-        int y1 = 5;
-        int x2 = 1;
-        int y2 = 0;
+        Point pointA = new Point(2, 5);
+        Point pointB = new Point(1, 0);
         double excepted = 5.09;
-        double result = Point.distance(x1, y1, x2, y2);
+        double result = pointA.distance(pointB);
         Assert.assertEquals(excepted, result, 0.01);
     }
 
     @Test
     public void when34to78then5dot65() {
-        int x1 = 3;
-        int y1 = 4;
-        int x2 = 7;
-        int y2 = 8;
+        Point pointA = new Point(3, 4);
+        Point pointB = new Point(7, 8);
         double excepted = 5.65;
-        double result = Point.distance(x1, y1, x2, y2);
+        double result = pointA.distance(pointB);
         Assert.assertEquals(excepted, result, 0.01);
     }
 
     @Test
     public void when22to08then6dot32() {
-        int x1 = 2;
-        int y1 = 2;
-        int x2 = 0;
-        int y2 = 8;
+        Point pointA = new Point(2, 2);
+        Point pointB = new Point(0, 8);
         double excepted = 6.32;
-        double result = Point.distance(x1, y1, x2, y2);
+        double result = pointA.distance(pointB);
         Assert.assertEquals(excepted, result, 0.01);
     }
 
     @Test
     public void whenNeg5Neg7to3Neg9then8dot24() {
-        int x1 = -5;
-        int y1 = -7;
-        int x2 = 3;
-        int y2 = -9;
+        Point pointA = new Point(-5, -7);
+        Point pointB = new Point(3, -9);
         double excepted = 8.24;
-        double result = Point.distance(x1, y1, x2, y2);
+        double result = pointA.distance(pointB);
         Assert.assertEquals(excepted, result, 0.01);
     }
 }
